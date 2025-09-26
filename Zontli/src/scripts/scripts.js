b83a9826a62e678a5e78c3765aad137ecc676419
dunkelheitEXE <alangmoreno02@gmail.com>
2025-09-25 22:24:20 -0600

    dev: sep 25 2025 - Angular Rebranding

---
+++ Zontli/src/scripts/scripts.js
@@ -0,0 +1,5 @@
+
+const offcanvas = document.getElementById('offcanvas');
+document.getElementById('btn-menu').addEventListener('click', function() {
+    offcanvas.classList.toggle('visible');
+});
