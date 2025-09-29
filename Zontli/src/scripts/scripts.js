bee5f6b94dda31af1d77a5061db8ecc7007bfb4e
dunkelheitEXE <alangmoreno02@gmail.com>
2025-09-28 21:43:27 -0600

    Redoing everything

--- Zontli/src/scripts/scripts.js
+++
@@ -1,5 +0,0 @@
-
-const offcanvas = document.getElementById('offcanvas');
-document.getElementById('btn-menu').addEventListener('click', function() {
-    offcanvas.classList.toggle('visible');
-});
