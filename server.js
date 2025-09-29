24962984a8e1148d9f2f8cdb5381b8d28c92be10
dunkelheitEXE <alangmoreno02@gmail.com>
2025-09-28 23:10:51 -0600

    dev: API finished and tested

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware for parsing JSON request bodies
+app.use(cors());
 app.use(express.json());
 
 // Define your API routes
